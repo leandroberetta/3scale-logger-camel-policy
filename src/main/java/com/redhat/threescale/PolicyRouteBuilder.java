df94d38a5bb5dc4c0ec7378915fd67ead8271c7c
Leandro Beretta <lea.beretta@gmail.com>
2021-01-25 15:53:04 -0300

    fix

--- src/main/java/com/redhat/threescale/PolicyRouteBuilder.java
+++ src/main/java/com/redhat/threescale/PolicyRouteBuilder.java
@@ -9,7 +9,7 @@ public class PolicyRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("netty4-http:proxy://0.0.0.0:8080")
-                .log("${headers.CamelHttpHost}")
+                .log("${headers.CamelHttpScheme}://${headers.CamelHttpHost}:${headers.CamelHttpPort}${headers.CamelHttpPath}")
                 .toD("netty4-http:"
                         + "${headers." + Exchange.HTTP_SCHEME + "}://"
                         + "${headers." + Exchange.HTTP_HOST + "}:"
